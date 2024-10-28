28235e32353f8b4eef2c18775d2cf36d76bae6d6
Minna Howell <minnah@google.com>
2024-10-28 20:03:13 +0000

    create query and add tests

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import (
 	"os"
 )
 
-const (
-	releaseEnvKey  = "CLOUD_DEPLOY_RELEASE"
-	projectEnvKey  = "CLOUD_DEPLOY_PROJECT"
-	locationEnvKey = "CLOUD_DEPLOY_LOCATION"
-	pipelineEnvKey = "CLOUD_DEPLOY_DELIVERY_PIPELINE"
-)
-
 func main() {
 	if err := do(); err != nil {
 		fmt.Printf("err: %v\n", err)
@@ -22,12 +15,4 @@ func main() {
 
 func do() error {
 	return nil
-}
-
-// Step 1. Get current  resources
-// Part a - prepare for a query
-// Execute query
-// Save results
-func envValue(envKey string) string {
-	return os.Getenv(envKey)
 }
