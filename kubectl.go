438cbac32adc218a059f3fe57de3ca6a98d432d7
Minna Howell <minnah@google.com>
2024-11-12 16:13:16 +0000

    clean up files

--- kubectl.go
+++ kubectl.go
@@ -14,37 +14,50 @@ const (
 	locationEnvKey    = "CLOUD_DEPLOY_LOCATION"
 	pipelineEnvKey    = "CLOUD_DEPLOY_DELIVERY_PIPELINE"
 	targetEnvKey      = "CLOUD_DEPLOY_TARGET"
-	outputNameArg     = "-o name"
+	outputFlag        = "-o"
+	nameArg           = "name"
 )
 
-// Get a list of resources that aren't in the current set of resources.
-func (ce CommandExecutor) getOldResources() ([]string, error) {
-	// Get a list of all resources on the cluster.
-	allResources, err := ce.getResources(false)
+// resourcesToDelete returns a list of resources that are not in the current set of resources
+// (i.e. the set of resources that were just deployed by Cloud Deploy in the most recent release).
+func (ce CommandExecutor) resourcesToDelete(namespace, resourceTypeFlag string) ([]string, error) {
+	// Step 1. Get a list of resource types to query.
+	resourceTypes, err := ce.resourceTypesToQuery(resourceTypeFlag)
 	if err != nil {
-		return nil, fmt.Errorf("TESTING failed to get a list of resources on the cluster, err: %w", err)
+		return nil, fmt.Errorf("failed to get a list of resources types to query, err: %w", err)
 	}
 
-	// Get a list of resources that were deployed as part of the latest release on the cluster.
-	currentResources, err := ce.getResources(true)
+	// Step 2. Get a list of all resources on the cluster that were deployed by Cloud Deploy.
+	allResources, err := ce.resources(false, namespace, resourceTypes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get a list of resources on the cluster, err: %w", err)
+	}
+
+	// Step 3. Get a list of resources that were deployed by Cloud Deploy as part of the latest
+	// release on the cluster.
+	currentResources, err := ce.resources(true, namespace, resourceTypes)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get a list of current resources on the cluster, err: %w", err)
 	}
 
+	// Step 4. Do a diff to determine what resources were not deployed in the latest release and
+	// should therefore be deleted.
 	return diffSlices(allResources, currentResources), nil
 }
 
+// apiResourceQueryArgs returns the args to pass to kubectl to get a list of supported resource
+// types on the cluster.
 func apiResourcesQueryArgs() []string {
 	return []string{
 		"api-resources",
 		"--verbs=list",
-		"-o",
-		"name",
+		outputFlag,
+		nameArg,
 	}
 }
 
-// kubectlGetArgs returns the get args.
-func kubectlGetArgs(includeReleaseLabel bool, resourceType string) []string {
+// kubectlGetArgs returns the args to pass to kubectl to get the resource name.
+func kubectlGetArgs(includeReleaseLabel bool, resourceType string, nspace string) []string {
 	var labels []string
 	if includeReleaseLabel {
 		labels = append(labels, fmt.Sprintf("%srelease-id=%s", cloudDeployPrefix, os.Getenv(releaseEnvKey)))
@@ -56,55 +69,76 @@ func kubectlGetArgs(includeReleaseLabel bool, resourceType string) []string {
 
 	labelsFormatted := strings.Join(labels, ",")
 	labelArg := fmt.Sprintf("-l %s", labelsFormatted)
-
-	return []string{
+	args := []string{
 		"get",
-		"-o",
-		"name",
+		outputFlag,
+		nameArg,
 		labelArg,
-		resourceType,
 	}
+	if nspace != "" {
+		args = append(args, fmt.Sprintf("--namespace=%s", nspace))
+	}
+	args = append(args, resourceType)
+
+	return args
 }
 
-// get kubernetes resources
-func (ce CommandExecutor) getResources(includeReleaseLabel bool) ([]string, error) {
-	apiResourcesArgs := apiResourcesQueryArgs()
-	// Not sure what the output looks like...long list? separated by new lines?
-	output, err := ce.execCommand(apiResourcesArgs)
-	if err != nil {
-		return nil, fmt.Errorf("failed to execute kubectl api-resources command: %w", err)
+// resourceTypesToQuery returns a list of resource types to query based on the command line flag value.
+func (ce CommandExecutor) resourceTypesToQuery(resourceType string) ([]string, error) {
+	var resourceTypes []string
+	if resourceType != "" {
+		resourceTypes = strings.Split(resourceType, ",")
+	} else {
+		apiResourcesArgs := apiResourcesQueryArgs()
+		output, err := ce.execCommand(apiResourcesArgs)
+		if err != nil {
+			return nil, fmt.Errorf("failed to execute kubectl api-resources command: %w", err)
+		}
+		outputSplit := strings.Split(output, "\n")
+		// Delete the empty line at the end
+		resourceTypes = slices.DeleteFunc(outputSplit, isEmpty)
 	}
-	temp := strings.Split(output, "\n")
-	// Delete the empty line at the end
-	boop := slices.DeleteFunc(temp, func(e string) bool {
-		return e == ""
-	})
+	return resourceTypes, nil
+}
 
-	// Loop over and get resources
-	// What if multiple resources
-	// What if not found
+// resources returns a list of resources, given a list of resource types to query on the cluster.
+func (ce CommandExecutor) resources(includeReleaseLabel bool, namespaces string, resourceTypes []string) ([]string, error) {
+
 	var resources []string
-	for _, resourceType := range boop {
-		args := kubectlGetArgs(includeReleaseLabel, resourceType)
+	for _, r := range resourceTypes {
+		res, err := ce.resourcesPerType(includeReleaseLabel, namespaces, r)
+		if err != nil {
+			return nil, fmt.Errorf("attempting to get resource type \"%v\" resulted in err: %w", r, err)
+		}
+		resources = append(resources, res...)
+	}
+	return resources, nil
+}
+
+// resourcesPerType returns a list of resources per type.
+func (ce CommandExecutor) resourcesPerType(includeReleaseLabel bool, namespaces string, resourceType string) ([]string, error) {
+	var resources []string
+	// Multiple namespaces could have been specified in the command line arg, split and loop through each.
+	nspaces := strings.Split(namespaces, ",")
+	for _, n := range nspaces {
+		args := kubectlGetArgs(includeReleaseLabel, resourceType, n)
 		output, err := ce.execCommand(args)
 		if err != nil {
 			return nil, fmt.Errorf("attempting to get resource type \"%v\" resulted in err: %w", resourceType, err)
 		}
 		if output != "" {
-			// Separate out by line break
-			temp := strings.Split(output, "\n")
-			slices.DeleteFunc(temp, func(e string) bool {
-				return e == ""
-			})
-			resources = append(resources, temp...)
+			// Separate out by line break and delete the empty line at the end.
+			outputSplit := strings.Split(output, "\n")
+			outputSplit = slices.DeleteFunc(outputSplit, isEmpty)
+			resources = append(resources, outputSplit...)
 		}
 	}
 	return resources, nil
+
 }
 
-// deleteResources deletes the resources given.
-func (ce CommandExecutor) DeleteResources(resources []string) error {
-	// Loop over and delete one by one
+// deleteResources deletes the given resources.
+func (ce CommandExecutor) deleteResources(resources []string) error {
 	fmt.Printf("Beginning to delete resources, there are %d resources to delete\n", len(resources))
 	for _, resource := range resources {
 		args := []string{"delete", resource}
@@ -116,257 +150,6 @@ func (ce CommandExecutor) DeleteResources(resources []string) error {
 	return nil
 }
 
-// // Get a list of resources that aren't in the current set of resources.
-// func (ce CommandExecutor) getOldResources() ([]string, error) {
-// 	// Get a list of all resources on the cluster.
-// 	allResources, err := ce.getResources(false)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Get a list of resources that were deployed as part of the latest release on the cluster.
-// 	currentResources, err := ce.getResources(true)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return diffSlices(allResources, currentResources), nil
-// }
-
-// // deleteResources deletes the resources given.
-// func (ce CommandExecutor) deleteResources(resources []string) error {
-// 	// Loop over and delete one by one
-// 	for _, resource := range resources {
-// 		args := []string{"delete", resource}
-// 		_, err := ce.execCommand(args)
-// 		if err != nil {
-// 			fmt.Printf("Attempting to delete resource %v resulted in err %v", resource, err)
-// 			return err
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func apiResourcesQueryArgs() []string {
-// 	return []string{
-// 		"api-resources",
-// 		"--verbs=list",
-// 		outputNameArg,
-// 	}
-// }
-
-// // kubectlGetArgs returns the get args.
-// func kubectlGetArgs(includeReleaseLabel bool) []string {
-// 	var labels []string
-// 	if includeReleaseLabel {
-// 		labels = append(labels, fmt.Sprintf("%srelease-id=%s", cloudDeployPrefix, os.Getenv(releaseEnvKey)))
-// 	}
-// 	labels = append(labels, fmt.Sprintf("%sdelivery-pipeline-id=%s", cloudDeployPrefix, os.Getenv(pipelineEnvKey)))
-// 	labels = append(labels, fmt.Sprintf("%starget-id=%s", cloudDeployPrefix, os.Getenv(targetEnvKey)))
-// 	labels = append(labels, fmt.Sprintf("%slocation=%s", cloudDeployPrefix, os.Getenv(locationEnvKey)))
-// 	labels = append(labels, fmt.Sprintf("%sproject-id=%s", cloudDeployPrefix, os.Getenv(projectEnvKey)))
-
-// 	labelsFormatted := strings.Join(labels, ",")
-// 	labelArg := fmt.Sprintf("-l %s", labelsFormatted)
-
-// 	return []string{
-// 		"get",
-// 		outputNameArg,
-// 		labelArg,
-// 	}
-// }
-
-// // get kubernetes resources
-// func (ce CommandExecutor) getResources(includeReleaseLabel bool) ([]string, error) {
-// 	apiResources := apiResourcesQueryArgs()
-// 	// Execute the API resources command to get the list of supported API resources
-// 	apiResourceCmd := exec.Command(ce.binPath, apiResources...)
-// 	// Get the API resource command's stdout and attach it to the kubectl get commands stdin.
-// 	apiOutputPipe, err := apiResourceCmd.StdoutPipe()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer apiOutputPipe.Close()
-
-// 	// kubectl get command
-// 	args := kubectlGetArgs(includeReleaseLabel)
-// 	kubectlGetCmd := exec.Command(ce.binPath, args...)
-// 	kubectlGetCmd.Stdin = apiOutputPipe
-
-// 	// Now run the api resource command
-// 	apiResourceCmd.Start()
-
-// 	// Run and get the output of kubectl get
-// 	result, err := kubectlGetCmd.Output()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	output := string(result)
-// 	var resources []string
-// 	if output != "" {
-// 		// Separate out by line break
-// 		temp := strings.Split(output, "\n")
-// 		resources = append(resources, temp...)
-// 		// for _, r := range temp {
-// 		// 	resources = append(resources, r)
-// 		// }
-// 	}
-// 	// Loop over and get resources
-// 	// What if multiple resources
-// 	// What if not found
-// 	// for _, resource := range apiResources {
-// 	// 	args := kubectlGetArgs(includeReleaseLabel, resource)
-// 	// 	output, err := ce.execCommand(args)
-// 	// 	if err != nil {
-// 	// 		fmt.Printf("Attempting to get resource %v resulted in err %v", resource, err)
-// 	// 		return nil, err
-// 	// 	}
-// 	// 	if output != "" {
-// 	// 		// Separate out by line break
-// 	// 		temp := strings.Split(output, "\n")
-// 	// 	}
-// 	// 	resources = append(resources, output)
-// 	// }
-// 	return resources, nil
-// }
-
-// // CommandExecutor contains command execution information.
-// // type CommandExecutor struct {
-// // 	// BinPath is the path the binary being used for the command (e.g. the path
-// // 	// to the kubectl binary if the kubectl command is to be used).
-// // 	binPath string
-// // }
-
-// // CreateCommandExecutor returns a CommandExecutor for the given binary
-// // func CreateCommandExecutor(binPath string) *CommandExecutor {
-// // 	ce := &CommandExecutor{
-// // 		binPath: binPath,
-// // 	}
-// // 	return ce
-// // }
-
-// // execCommand runs the given command and returns the output.
-// func (ce CommandExecutor) execCommand(args []string) (string, error) {
-// 	fmt.Printf("Running the following command: %s %s\n", ce.binPath, args)
-// 	cmd := exec.Command(ce.binPath, args...)
-// 	// By default set locations to standard error and output (visible in cloud build logs)
-// 	cmd.Stderr = os.Stderr
-// 	cmd.Stdout = os.Stdout
-
-// 	// Write error output to two locations simultaneously. Unless disabled by a CommandOption, this will allow
-// 	// us to see the error output as the execution is happening (by writing to standard error) and also allow us
-// 	// to gather all stderr at the end (by also writing to var stderr).
-// 	var stderr bytes.Buffer
-// 	errWriter := io.MultiWriter(&stderr, cmd.Stderr)
-// 	cmd.Stderr = errWriter
-
-// 	// Write output to two locations simultaneously. Unless disabled by a CommandOption, this will allow
-// 	// us to see the output as the execution is happening (by writing to standard out) and also allow us
-// 	// to gather all stdout at the end (by also writing to var stdout).
-// 	var stdout bytes.Buffer
-// 	outWriter := io.MultiWriter(&stdout, cmd.Stdout)
-// 	cmd.Stdout = outWriter
-
-// 	// Start the command
-// 	if err := cmd.Start(); err != nil {
-// 		return "", fmt.Errorf("failed to start command, err is %w", err)
-// 	}
-
-// 	// Wait for everything to finish.
-// 	if err := cmd.Wait(); err != nil {
-// 		// Read the stdErr output
-// 		errorOutput := stderr.Bytes()
-// 		fullErr := fmt.Errorf("error running command: %w\n%s", err, errorOutput)
-// 		return "", fullErr
-// 	}
-// 	return stdout.String(), nil
-// }
-
-// // diffSlices returns the elements in slice1 that aren't in slice2.
-// func diffSlices(slice1, slice2 []string) []string {
-// 	var diff []string
-// 	set := make(map[string]bool)
-
-// 	for _, val := range slice2 {
-// 		set[val] = true
-// 	}
-
-// 	for _, val := range slice1 {
-// 		// If they're not in slice 2
-// 		if !set[val] {
-// 			diff = append(diff, val)
-// 		}
-// 	}
-
-// 	return diff
-// }
-
-// TRASH
-// get kubernetes resources
-// func (ce CommandExecutor) getResources(includeReleaseLabel bool) ([]string, error) {
-// 	apiResources := apiResourcesQueryArgs()
-// 	// Execute the API resources command to get the list of supported API resources
-// 	apiResourceCmd := exec.Command(ce.binPath, apiResources...)
-// 	// Get the API resource command's stdout and attach it to the kubectl get commands stdin.
-// 	apiOutputPipe, err := apiResourceCmd.StdoutPipe()
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get stdout pipe from kubectl api-resources command: %w", err)
-// 	}
-// 	defer apiOutputPipe.Close()
-
-// 	// kubectl get command
-// 	args := kubectlGetArgs(includeReleaseLabel)
-// 	kubectlGetCmd := exec.Command(ce.binPath, args...)
-// 	kubectlGetCmd.Stdin = apiOutputPipe
-
-// 	// Write error output to multiple places for api command
-// 	var stderr bytes.Buffer
-// 	errWriter := io.MultiWriter(&stderr, apiResourceCmd.Stderr)
-// 	apiResourceCmd.Stderr = errWriter
-
-// 	// Write error output to multiple places for api command
-// 	var stderr2 bytes.Buffer
-// 	errWriter2 := io.MultiWriter(&stderr2, kubectlGetCmd.Stderr)
-// 	kubectlGetCmd.Stderr = errWriter2
-
-// 	// Now run the api resource command
-// 	if err := apiResourceCmd.Start(); err != nil {
-// 		return nil, fmt.Errorf("failed to execute kubectl api-resources command: %w", err)
-// 	}
-
-// 	// Run and get the output of kubectl get
-// 	result, err := kubectlGetCmd.Output()
-// 	output := string(result)
-// 	if err != nil {
-// 		errorOutput := stderr2.Bytes()
-
-// 		return nil, fmt.Errorf("failed to execute kubectl get command: %w, output: %v, full error: %s", err, output, errorOutput)
-// 	}
-
-// 	var resources []string
-// 	if output != "" {
-// 		// Separate out by line break
-// 		temp := strings.Split(output, "\n")
-// 		resources = append(resources, temp...)
-// 		// for _, r := range temp {
-// 		// 	resources = append(resources, r)
-// 		// }
-// 	}
-// 	// Loop over and get resources
-// 	// What if multiple resources
-// 	// What if not found
-// 	// for _, resource := range apiResources {
-// 	// 	args := kubectlGetArgs(includeReleaseLabel, resource)
-// 	// 	output, err := ce.execCommand(args)
-// 	// 	if err != nil {
-// 	// 		fmt.Printf("Attempting to get resource %v resulted in err %v", resource, err)
-// 	// 		return nil, err
-// 	// 	}
-// 	// 	if output != "" {
-// 	// 		// Separate out by line break
-// 	// 		temp := strings.Split(output, "\n")
-// 	// 	}
-// 	// 	resources = append(resources, output)
-// 	// }
-// 	return resources, nil
-// }
+func isEmpty(e string) bool {
+	return e == ""
+}
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"bytes"
+	"flag"
 	"fmt"
-	"io"
 	"os"
-	"os/exec"
+)
+
+var (
+	namespace = flag.String("namespace", "", "Namespace(s) to filter on when finding resources to delete. "+
+		"For multiple namespaces, separate them with a comma. For example --namespace=foo,bar.")
+	resourceType = flag.String("resource-type", "", "Resource type(s) to filter on when finding resources to delete. "+
+		"If listing multiple resource types, separate them with commas. For example, --resource-type=Deployment,Job. "+
+		"You can also qualify the resource type by an API group if you want to specify resources only in a specific "+
+		"API group. For example --resource-type=deployments.apps")
 )
 
 func main() {
+	flag.Parse()
+
 	if err := do(); err != nil {
 		fmt.Printf("err: %v\n", err)
 		os.Exit(1)
@@ -18,88 +27,17 @@ func main() {
 }
 
 func do() error {
-	// Step 1 get old resources
+	// Step 1. Get a list of resources to delete.
 	kubectlExec := CreateCommandExecutor("kubectl")
-	oldResources, err := kubectlExec.getOldResources()
+	oldResources, err := kubectlExec.resourcesToDelete(*namespace, *resourceType)
 	if err != nil {
 		return err
 	}
 
-	// Step 2 delete them
-	if err := kubectlExec.DeleteResources(oldResources); err != nil {
+	// Step 2. Delete the resources.
+	if err := kubectlExec.deleteResources(oldResources); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-// CommandExecutor contains command execution information.
-type CommandExecutor struct {
-	// BinPath is the path the binary being used for the command (e.g. the path
-	// to the kubectl binary if the kubectl command is to be used).
-	binPath string
-}
-
-// CreateCommandExecutor returns a CommandExecutor for the given binary
-func CreateCommandExecutor(binPath string) *CommandExecutor {
-	ce := &CommandExecutor{
-		binPath: binPath,
-	}
-	return ce
-}
-
-// execCommand runs the given command and returns the output.
-func (ce CommandExecutor) execCommand(args []string) (string, error) {
-	fmt.Printf("Running the following command: %s %s\n", ce.binPath, args)
-	cmd := exec.Command(ce.binPath, args...)
-	// By default set locations to standard error and output (visible in cloud build logs)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-
-	// Write error output to two locations simultaneously. Unless disabled by a CommandOption, this will allow
-	// us to see the error output as the execution is happening (by writing to standard error) and also allow us
-	// to gather all stderr at the end (by also writing to var stderr).
-	var stderr bytes.Buffer
-	errWriter := io.MultiWriter(&stderr, cmd.Stderr)
-	cmd.Stderr = errWriter
-
-	// Write output to two locations simultaneously. Unless disabled by a CommandOption, this will allow
-	// us to see the output as the execution is happening (by writing to standard out) and also allow us
-	// to gather all stdout at the end (by also writing to var stdout).
-	var stdout bytes.Buffer
-	outWriter := io.MultiWriter(&stdout, cmd.Stdout)
-	cmd.Stdout = outWriter
-
-	// Start the command
-	if err := cmd.Start(); err != nil {
-		return "", fmt.Errorf("failed to start command, err is %w", err)
-	}
-
-	// Wait for everything to finish.
-	if err := cmd.Wait(); err != nil {
-		// Read the stdErr output
-		errorOutput := stderr.Bytes()
-		fullErr := fmt.Errorf("error running command: %w\n%s", err, errorOutput)
-		return "", fullErr
-	}
-	return stdout.String(), nil
-}
-
-// diffSlices returns the elements in slice1 that aren't in slice2.
-func diffSlices(slice1, slice2 []string) []string {
-	var diff []string
-	set := make(map[string]bool)
-
-	for _, val := range slice2 {
-		set[val] = true
-	}
-
-	for _, val := range slice1 {
-		// If they're not in slice 2
-		if !set[val] {
-			diff = append(diff, val)
-		}
-	}
-
-	return diff
 }
